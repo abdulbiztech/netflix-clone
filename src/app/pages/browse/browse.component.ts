d3d9629f7ebd31c6d15abd8f52bdc3c7a5961c12
Abdul Karim <abdulbiztech@gmail.com>
2024-01-04 14:03:29 +0530

    card displayed using pipes

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,25 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
 import { HeaderComponent } from 'src/app/core/components/header/header.component';
 import { BannerComponent } from 'src/app/core/components/banner/banner.component';
+import { MovieService } from 'src/app/shared/services/movie.service';
+import { MovieCarouselComponent } from 'src/app/shared/components/movie-carousel/movie-carousel.component';
+import { IVideoContent } from 'src/app/shared/models/video-contents-interface';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, BannerComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    BannerComponent,
+    MovieCarouselComponent,
+  ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css',
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+  movieService = inject(MovieService);
   private router = inject(Router);
   auth = inject(AuthService);
   name = JSON.parse(sessionStorage.getItem('loggedInUser')!).name;
   email = JSON.parse(sessionStorage.getItem('loggedInUser')!).email;
   picture = JSON.parse(sessionStorage.getItem('loggedInUser')!).picture;
-
+  popularMovies: IVideoContent[] = [];
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe(
+      (resp: any) => {
+        console.log(resp);
+        this.popularMovies = resp.results;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
   logout() {
+    sessionStorage.removeItem('loggedInUser');
     this.auth.SignOut();
   }
 }
---
+++ src/app/shared/components/movie-carousel/movie-carousel.component.ts
@@ -0,0 +1,71 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import Swiper from 'swiper';
+import { IVideoContent } from '../../models/video-contents-interface';
+import { DescriptionPipe } from '../../pipes/description.pipe';
+import { ImagePipe } from '../../pipes/image.pipe';
+
+@Component({
+  selector: 'app-movie-carousel',
+  standalone: true,
+  imports: [CommonModule, DescriptionPipe, ImagePipe],
+  templateUrl: './movie-carousel.component.html',
+  styleUrl: './movie-carousel.component.css',
+})
+export class MovieCarouselComponent implements OnInit, AfterViewInit {
+  @Input() videoContents: IVideoContent[] = [];
+  @Input() title!: string;
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
+
+  private initSwiper() {
+    return new Swiper(this.swiperContainer.nativeElement, {
+      slidesPerView: 3,
+      slidesPerGroup: 2,
+      centeredSlides: true,
+      loop: true,
+      breakpoints: {
+        600: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        900: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1500: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1800: {
+          slidesPerView: 5,
+          slidesPerGroup: 6,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+      },
+    });
+  }
+}
