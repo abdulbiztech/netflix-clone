6f8d70f50130b26239b58986eb86fc65434004e7
abdulbiztech <abdulbiztech@gmail.com>
2024-01-05 16:46:05 +0530

    new change

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,24 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { MoviesService } from './../../shared/services/movies.service';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
 import { HeaderComponent } from 'src/app/core/components/header/header.component';
 import { BannerComponent } from 'src/app/core/components/banner/banner.component';
+import { MovieCarouselComponent } from 'src/app/shared/components/movie-carousel/movie-carousel.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, BannerComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    BannerComponent,
+    MovieCarouselComponent,
+  ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css',
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+  MoviesService = inject(MoviesService);
   private router = inject(Router);
   auth = inject(AuthService);
   name = JSON.parse(sessionStorage.getItem('loggedInUser')!).name;
   email = JSON.parse(sessionStorage.getItem('loggedInUser')!).email;
   picture = JSON.parse(sessionStorage.getItem('loggedInUser')!).picture;
-
+  ngOnInit(): void {
+    this.MoviesService.getMovies().subscribe((res) => {
+      console.log('response', res);
+    });
+  }
   logout() {
     this.auth.SignOut();
   }
